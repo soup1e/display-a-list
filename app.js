3784af9ced4d6542dc09d8e85e25ef4e152ce5eb
Sam Adams <sam.adams7785@gmail.com>
2022-09-15 12:58:07 -0700

    added people and sport list

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ import { renderFood } from './render-foods.js';
 import { animals } from './data-animals.js';
 import { renderAnimal } from './render-animals.js';
 
+import { sports } from './data-sports.js';
+import { renderSport } from './render-sports.js';
+
+import { people } from './data-people.js';
+import { renderPeople } from './render-people.js';
+
 const foodList = document.getElementById('foods-list');
 
 function displayFoods() {
@@ -26,5 +32,29 @@ function displayAnimal() {
     }
 }
 
+const sportList = document.getElementById('sports-list');
+
+function displaySports() {
+    sportList.innerHTML = '';
+
+    for (let sport of sports) {
+        const sportEl = renderSport(sport);
+        sportList.append(sportEl);
+    }
+}
+
+const peopleList = document.getElementById('people-list');
+
+function displayPerson() {
+    peopleList.innerHTML = '';
+
+    for (let person of people) {
+        const personEl = renderPeople(person);
+        peopleList.append(personEl);
+    }
+}
+
+displayPerson();
 displayFoods();
 displayAnimal();
+displaySports();
---
+++ data-sports.js
@@ -0,0 +1,17 @@
+export const sports = [
+    {
+        name: 'Baseball',
+        equipment: 'Ball Mitt Bat',
+        players: '9',
+    },
+    {
+        name: 'basketball',
+        equipment: 'Ball',
+        players: '5',
+    },
+    {
+        name: 'football',
+        equipment: 'Ball Protection',
+        players: '11',
+    },
+];
